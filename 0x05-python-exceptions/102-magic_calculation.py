ce7e907454ffa66369e957f4c36aa31c89d705b9
fahad-moha <khiladifahad02@gmail.com>
2023-10-23 04:15:35 -0700

    updated

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
+        except ZeroDivisionError:
+            result = float('inf')
         except Exception as e:
             result = b + a
             break
