51494416500660304386d874f10bda8d0cb95d4b
fahad-moha <khiladifahad02@gmail.com>
2023-11-12 02:56:13 -0800

    updated with Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
         Args:
-            size (int): The size of the new Square.
+            size (int): The size of the fresh Square.
             x (int): The x coordinate of the new Square.
             y (int): The y coordinate of the new Square.
             id (int): The identity of the new Square.
